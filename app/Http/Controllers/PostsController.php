ef72c1e1a64dc753fcb379e9bec4728728eca3bb
Josh Green <joshua.kasyoka.green@gmail.com>
2021-06-30 22:25:33 +0100

    updates

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
+    public function construct()
+    {
+        $this->middleware('auth');
+    }
+
+
     public function create()
     {
         return view('posts.create');
@@ -20,9 +26,16 @@ class PostsController extends Controller
             'image' => ['required', 'image'],
         ]);
 
-        auth()->user()->posts()->create($data);
+        $imagePath = request('image')->store('uploads', 'public');
 
-        dd(request()->all());
+        auth()->user()->posts()->create([
+            'caption' => $data['caption'],
+            'image' => $imagePath,
+        ]);
+
+        return redirect('/profile/' . auth()->user()->id);
+
+
     }
 
 }
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -10,10 +10,10 @@
             <div class="d-flex justify-content-between align-items-baseline
 ">
             <div><h1>{{$user->username}}</h1></div>
-            <a href="#">Add New Post</a>
+            <a href="/p/create">Add New Post</a>
             </div>
             <div class="d-flex">
-                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
                 <div class="pr-5"><strong>23k</strong> followers</div>
                 <div class="pr-5"><strong>212</strong> following</div>
             </div>
@@ -24,15 +24,11 @@
     </div>
 
     <div class="row pt-5">
-        <div class="col-4">
-            <img src="https://s3.amazonaws.com/www-inside-design/uploads/2020/10/aspect-ratios-blogpost-1x1-1.png" alt="" class="w-100">
+        @foreach($user->posts as $post)
+        <div class="col-4 pb-4">
+            <img src="/storage/{{ $post->image }}" class="w-100">
         </div>
-        <div class="col-4">
-            <img src="https://blog.uniclixapp.com/wp-content/uploads/2020/05/1-1-instagram-1024x1024-1.jpg" alt="" class="w-100">
-        </div>
-        <div class="col-4">
-            <img src="https://steamuserimages-a.akamaihd.net/ugc/924802058717094712/ACC7BE614DDB7472BB466F8E53FF75368C3C6E7F/?imw=512&imh=512&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true" alt="" class="w-100">
-        </div>
+        @endforeach
     </div>
 </div>
 @endsection
